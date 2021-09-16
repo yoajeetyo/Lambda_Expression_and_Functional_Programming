9b0b3b0ea4c48a3df12fee71becbf3601be119b1
yoajeetyo <sajeetkumar40@gmail.com>
2021-09-17 03:42:17 +0530

    Added : static method used in functional interface and passing lambda object through main class

--- src/com/bl/functionalinterface/MathOperationApp.java
+++ src/com/bl/functionalinterface/MathOperationApp.java
@@ -3,6 +3,9 @@ package com.bl.functionalinterface;
 @FunctionalInterface
 interface IMathFunction{
 	int calculate(int a , int b);
+	static void printResult(int a , int b , String function , IMathFunction fobj) {
+		System.out.println("The result of "+function+" is : "+fobj.calculate(a, b));
+	}
 }
 
 public class MathOperationApp {
@@ -12,8 +15,12 @@ public static void main(String[] args) {
 	IMathFunction mulFunction = (x,y)->x*y;
 	IMathFunction divFunction = (x,y)->x/y;
 	
-	System.out.println("Addition is : "+ addFunction.calculate(16 , 6));
-	System.out.println("Multiplication is : "+ mulFunction.calculate(4 , 6));
-	System.out.println("Division is : "+ divFunction.calculate(8 , 4));
+	//System.out.println("Addition is : "+ addFunction.calculate(16 , 6));
+	//System.out.println("Multiplication is : "+ mulFunction.calculate(4 , 6));
+	//System.out.println("Division is : "+ divFunction.calculate(8 , 4));
+	
+	IMathFunction.printResult(20, 4, "Addition", addFunction);
+	IMathFunction.printResult(20, 4, "Multiplication", mulFunction);
+	IMathFunction.printResult(20, 4, "Division", divFunction);
 }
 }
