05593121ff6062431f4dd910e126aa5057e91ca3
yoajeetyo <sajeetkumar40@gmail.com>
2021-09-16 18:25:58 +0530

    Added : Calcuulator Application Using Lambda

---
+++ src/com/bl/functionalinterface/MathOperationApp.java
@@ -0,0 +1,19 @@
+package com.bl.functionalinterface;
+
+@FunctionalInterface
+interface IMathFunction{
+	int calculate(int a , int b);
+}
+
+public class MathOperationApp {
+	
+public static void main(String[] args) {
+	IMathFunction addFunction = Integer::sum;
+	IMathFunction mulFunction = (x,y)->x*y;
+	IMathFunction divFunction = (x,y)->x/y;
+	
+	System.out.println("Addition is : "+ addFunction.calculate(16 , 6));
+	System.out.println("Multiplication is : "+ mulFunction.calculate(4 , 6));
+	System.out.println("Division is : "+ divFunction.calculate(8 , 4));
+}
+}
